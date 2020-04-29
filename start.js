b8193e4893b4074418595e0ad7bbf194bf06ab42
tspng <tspng@socket.ch>
2020-04-29 23:52:33 +0200

    Use process.env for getting port by default

--- start.js
+++ start.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const app = require("./app/server");
 
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
 // serve static files (frontend) from public/
 app.use(express.static("public"));
